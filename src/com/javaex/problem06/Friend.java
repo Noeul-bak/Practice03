92cfe2cef242e723f4956441480f8e73ac39c059
lg <lg@192.168.0.7>
2017-07-09 23:32:15 +0900

    6하는중

--- src/com/javaex/problem06/Friend.java
+++ src/com/javaex/problem06/Friend.java
@@ -9,7 +9,18 @@ public class Friend {
     
     //필요한 메소드 작성
     
+    public void setName(String name) {
+    	this.name=name;
+    }
     
+    public void setHp(String hp) {
+    	this.hp=hp;
+    }
+    
+    public void setSchool(String school) {
+    	this.school=school;
+    }
+        
     
     public void showInfo(){
         System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
