4af5f959760d6007a3186724962207b6d6ae52d5
lg <lg@192.168.0.7>
2017-07-09 21:55:07 +0900

    8풀이중

--- src/com/javaex/problem08/Book.java
+++ src/com/javaex/problem08/Book.java
@@ -1,8 +1,54 @@
 package com.javaex.problem08;
 
 public class Book {
+	
+	public int bookNo;
+	public String title;
+	public String author;
+	public int stateCode;
+	
+	public Book(int bookNo, String title, String author) {
+		this.bookNo=bookNo;
+		this.title=title;
+		this.author=author;
+		stateCode=1;
+	}
+	
+
     
+	public void setBookNum(int bookNo) {
+		this.bookNo=bookNo;
+	}
+	public int getBookNum(){
+		return bookNo;
+	}
+	
+	public void setTitle(String title) {
+		this.title=title;
+	}
+	public String getTitle(){
+		return title;
+	}
+	public void setAuthor(String author) {
+		this.author=author;
+	}
+	public String getAuthor() {
+		return author;
+	}
     
+	
+	
+	public void rent(int stateCode) {
+		stateCode=0;
+		System.out.println(title+"이(가) 대여 됐습니다.");
+	}
+	public void print() {
+		if(stateCode==1) {
+			System.out.println(bookNo+"  책 제목 :"+title+",	작가:"+author+",	대여유무: 재고있음");
+		} else if(stateCode==0) {
+			System.out.println(bookNo+"  책 제목 :"+title+",	작가:"+author+",	대여유무: 대여중");
+		}
+	}
     
     
     
